2b8d5eec3e714a334ad949f4deb31deed4d81c6a
nkcsgexi <xge.ncsu2@gmail.com>
2013-08-19 14:55:29 -0400

    rename method detector

--- src/edu/dlf/refactoring/detectors/RenameMethodDetector.java
+++ src/edu/dlf/refactoring/detectors/RenameMethodDetector.java
@@ -1,16 +1,22 @@
 package edu.dlf.refactoring.detectors;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.SimpleName;
+
 import com.google.inject.Inject;
 
 import edu.dlf.refactoring.change.ChangeComponentInjector.MethodDeclarationAnnotation;
 import edu.dlf.refactoring.change.ChangeComponentInjector.MethodInvocationAnnotation;
-import edu.dlf.refactoring.change.ChangeComponentInjector.QualifiedNameAnnotation;
 import edu.dlf.refactoring.change.ChangeComponentInjector.SimpleNameAnnotation;
 import edu.dlf.refactoring.design.IRefactoring;
 import edu.dlf.refactoring.design.ISourceChange;
 import edu.dlf.refactoring.design.ISourceChange.SourceChangeType;
 import edu.dlf.refactoring.detectors.SourceChangeSearcher.IChangeSearchCriteria;
 import edu.dlf.refactoring.detectors.SourceChangeSearcher.IChangeSearchResult;
+import edu.dlf.refactoring.refactorings.RenameMethodRefactoring;
+import fj.Equal;
+import fj.F;
 import fj.data.List;
+import fj.data.List.Buffer;
 
 public class RenameMethodDetector extends AbstractRefactoringDetector{
 	private final IChangeSearchCriteria declarationChangeCriteira;
@@ -19,7 +25,6 @@ public class RenameMethodDetector extends AbstractRefactoringDetector{
 	@Inject
 	public RenameMethodDetector(
 			@SimpleNameAnnotation String snChangeLevel,
-			@QualifiedNameAnnotation String qnChangeLevel,
 			@MethodDeclarationAnnotation String mdChangeLevel,
 			@MethodInvocationAnnotation String miChangeLevel)
 	{
@@ -29,9 +34,22 @@ public class RenameMethodDetector extends AbstractRefactoringDetector{
 					getSearchCriteria();
 		this.invocationChangeCriteira = this.getCascadeCriteriaBuilder().
 			addSingleChangeCriteria(miChangeLevel, SourceChangeType.PARENT).
-				addZeroOrMoreChangeCriteria(qnChangeLevel, SourceChangeType.PARENT).
-					addSingleChangeCriteria(snChangeLevel, SourceChangeType.UPDATE).
-						getSearchCriteria();
+				addSingleChangeCriteria(snChangeLevel, SourceChangeType.UPDATE).
+					getSearchCriteria();
+	}
+	
+	private final SimpleName getLastChangeBeforeName(IChangeSearchResult result){
+		return (SimpleName) result.getSourceChanges().last().getNodeBefore();
+	}
+	private final SimpleName getLastChangeAfterName(IChangeSearchResult result){
+		return (SimpleName) result.getSourceChanges().last().getNodeAfter();
+	}
+	
+	private final boolean areBindingSame(SimpleName name1, SimpleName name2)
+	{
+		if(name1.resolveBinding() == null || name2.resolveBinding() == null)
+			return false;
+		return name1.resolveBinding().getKey().equals(name2.resolveBinding().getKey());
 	}
 	
 	@Override
@@ -40,9 +58,39 @@ public class RenameMethodDetector extends AbstractRefactoringDetector{
 		List<IChangeSearchResult> invChanges = this.invocationChangeCriteira.search(change);
 		if(decChanges.isEmpty() && invChanges.isEmpty())
 			return List.nil();
+		Buffer<IRefactoring> buffer = Buffer.empty();
 		
+		// Rename from a declared method
+		return decChanges.append(invChanges).filter(new F<IChangeSearchResult, Boolean>(){
+			@Override
+			public Boolean f(IChangeSearchResult result) {
+				return getLastChangeBeforeName(result).resolveBinding() != null ||
+						getLastChangeAfterName(result).resolveBinding() != null;
+			}}).group(Equal.equal(new F<IChangeSearchResult, F<IChangeSearchResult, Boolean>>(){
+				@Override
+				public F<IChangeSearchResult, Boolean> f(final IChangeSearchResult r1) {
+					return new F<IChangeSearchResult, Boolean>(){
+						@Override
+						public Boolean f(IChangeSearchResult r2) {
+							return areBindingSame(getLastChangeBeforeName(r1), getLastChangeBeforeName(r2)) ||
+									areBindingSame(getLastChangeAfterName(r1), getLastChangeAfterName(r2));
+						}};
+				}})).map(new F<List<IChangeSearchResult>, IRefactoring>(){
+					@Override
+					public IRefactoring f(List<IChangeSearchResult> results) {
+						List<ASTNode> namesBefore = results.map(new F<IChangeSearchResult, ASTNode>(){
+							@Override
+							public ASTNode f(IChangeSearchResult result) {
+								return getLastChangeBeforeName(result);
+							}});
+						List<ASTNode> namesAfter = results.map(new F<IChangeSearchResult, ASTNode>(){
+							@Override
+							public ASTNode f(IChangeSearchResult result) {
+								return getLastChangeAfterName(result);
+							}});
+						return new RenameMethodRefactoring(namesBefore, namesAfter);				
+					}});
 		
-		return List.nil();
 	}
 
 }
--- src/edu/dlf/refactoring/refactorings/RenameMethodRefactoring.java
+++ src/edu/dlf/refactoring/refactorings/RenameMethodRefactoring.java
@@ -1,14 +1,14 @@
 package edu.dlf.refactoring.refactorings;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+
 import edu.dlf.refactoring.design.RefactoringType;
+import fj.data.List;
 
 
 public class RenameMethodRefactoring extends AbstractRefactoring{
 
-	public RenameMethodRefactoring() {
-		super(RefactoringType.RenameMethod);
-		
+	public RenameMethodRefactoring(List<ASTNode> namesBefore, List<ASTNode> namesAfter) {
+		super(RefactoringType.RenameMethod);	
 	}
-	
-
 }
